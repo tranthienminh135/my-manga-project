c4200a5ef2e17f6fe519454288b5883c2353022c
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-12-22 14:24:17 +0700

    update

--- src/ui/home/home-component.tsx
+++ src/ui/home/home-component.tsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getPlaylistsData } from '../../core/redux/slice/playlists-slice';
+
 export default function Home() {
-    return <div>Home</div>;
+    const youtubePlaylistsRedux = useSelector(getPlaylistsData);
+    const [youtubePlaylistsState, setYoutubePlaylistsState] = useState<any>();
+
+    useEffect(() => {
+        if (youtubePlaylistsRedux && youtubePlaylistsRedux.items.length > 0) {
+            setYoutubePlaylistsState(youtubePlaylistsRedux.items);
+        }
+    }, [youtubePlaylistsRedux]);
+
+    return (
+        <div>
+            <ul>
+                {youtubePlaylistsState &&
+                    youtubePlaylistsState.length > 0 &&
+                    youtubePlaylistsState.map((data: any) => {
+                        return <li key={data.id}>{data.snippet.title}</li>;
+                    })}
+            </ul>
+        </div>
+    );
 }
