257c9f1a32e128d327afda931ad05bc8c679f7d1
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-12-22 14:42:40 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+document.onkeydown = KeyCheck;
+function KeyCheck(event: any) {
+    var KeyID = event.keyCode;
+    if (KeyID === 116) {
+        const a = document.createElement('a');
+        a.href = window.location.href;
+        a.click();
+        return false;
+    }
+}
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <GoogleOAuthProvider clientId="294461961427-u1lan3dkbgbi5b5vmt27s3va3hg0hrkl.apps.googleusercontent.com">
+                <GoogleOAuthProvider
+                    clientId={process.env.REACT_APP_YOUTUBE_CLIENT_ID ? process.env.REACT_APP_YOUTUBE_CLIENT_ID : ''}
+                >
                     <App />
                 </GoogleOAuthProvider>
             </BrowserRouter>
