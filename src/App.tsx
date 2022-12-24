caf0fe7598d096873bf2aeb60fee44d752ebb207
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-12-24 16:33:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.scss';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Suspense } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import './App.scss';
 import RenderRouter from './route/route';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
