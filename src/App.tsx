5a4ce83df7f64a2784c24d7eb9e2675992709317
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-12-24 16:32:07 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,6 @@ import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: 'AIzaSyCmYIyaCXUNns8tDmeMH5l8oJVDq8-G6mE',
-    authDomain: 'manga-2416d.firebaseapp.com',
-    projectId: 'manga-2416d',
-    storageBucket: 'manga-2416d.appspot.com',
-    messagingSenderId: '126846395551',
-    appId: '1:126846395551:web:06780760f5a1117e0baeaa',
-    measurementId: 'G-7JQCZCVE6L',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
-
 const queryClient = new QueryClient();
 function App() {
     return (
