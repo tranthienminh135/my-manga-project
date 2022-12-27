40b9527ef067655b5c1a353794a22f59a3ab4572
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-12-27 15:41:48 +0700

    update

--- src/ui/facebook/comment-facebook-component.tsx
+++ src/ui/facebook/comment-facebook-component.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 export type CommentFacebookProps = {
     currentHref: string;
 };
@@ -5,10 +7,33 @@ export type CommentFacebookProps = {
 export default function CommentFacebook(props: CommentFacebookProps) {
     const { currentHref } = props;
 
-    const dom: any = window;
-    if (dom.FB) {
-        dom.FB.XFBML.parse();
-    }
+    useEffect(() => {
+        const dom: any = window;
+        if (dom.FB) {
+            dom.FB.XFBML.parse();
+
+            dom.fbAsyncInit = function () {
+                dom.FB.init({
+                    appId: '699076034884083',
+                    autoLogAppEvents: true,
+                    xfbml: true,
+                    version: 'v15.0',
+                });
+            };
+
+            // Load the SDK asynchronously
+            (function (d, s, id) {
+                var js: any,
+                    fjs: any = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src =
+                    'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v15.0&appId=699076034884083&autoLogAppEvents=1';
+                fjs.parentNode.insertBefore(js, fjs);
+            })(document, 'script', 'facebook-jssdk');
+        }
+    }, []);
 
     return (
         <>
