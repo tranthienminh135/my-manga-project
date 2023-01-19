3e3e874627a6fe52424aee47b55ffedc6aa5f170
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2023-01-19 18:54:48 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ import { store } from './core/redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import BasicSpeedDial from './shared-components/speed-dial/basic-speed-dial';
+import { ThemeProvider } from '@mui/material';
+import { customTheme } from './customTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -18,8 +20,10 @@ root.render(
                 <GoogleOAuthProvider
                     clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID ? process.env.REACT_APP_GOOGLE_CLIENT_ID : ''}
                 >
-                    <BasicSpeedDial />
-                    <App />
+                    <ThemeProvider theme={customTheme}>
+                        <BasicSpeedDial />
+                        <App />
+                    </ThemeProvider>
                 </GoogleOAuthProvider>
             </BrowserRouter>
         </Provider>
--- src/ui/content/content-component.tsx
+++ src/ui/content/content-component.tsx
@@ -90,7 +90,6 @@ export default function Content() {
                     )}
                     <Sidebar archives={youtubePlaylistsState ? youtubePlaylistsState.items : []} />
                 </Grid>
-                {`${window.location.href}/${playlistsDetailState.id}`}
                 <Grid container sx={{ mt: 3 }}>
                     <CommentFacebook currentHref={`${window.location.href}/${playlistsDetailState.id}`} />
                 </Grid>
